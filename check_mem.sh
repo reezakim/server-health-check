bcdb2bd4cceaabea5deaa88ac62d63292f99b823
reezakim <rezahakimarafah23@gmail.com>
2026-07-12 23:03:47 +0700

    day3: add memory usage percentage calculation

--- check_mem.sh
+++ check_mem.sh
@@ -4,5 +4,3 @@ total_mem=$(free -m | grep "Mem" | awk '{print $2}')
 usage_mem=$(free -m | grep "Mem" | awk '{print $3}')
 
 echo "Penggunaan Memori anda: $(( $usage_mem * 100 / $total_mem ))%"
-echo "Penggunaan Memori anda: $(( $usage_mem / $total_mem * 100 ))"
-
